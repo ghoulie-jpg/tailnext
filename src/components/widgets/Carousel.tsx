7dd70bf834339ec86931db06b7b72b6c1b96aa7e
ghoulie-jpg <104750700+ghoulie-jpg@users.noreply.github.com>
2025-04-04 12:42:35 -0400

    Update Carousel.tsx

--- src/components/widgets/Carousel.tsx
+++ src/components/widgets/Carousel.tsx
@@ -7,7 +7,6 @@ export function CarouselCustomNavigation() {
   return (
     <div className="custom-carousel-container w-30 h-30"> {/* Added w-96 h-64 here */}
     <Carousel
-      className="rounded-xl"
       navigation={({ setActiveIndex, activeIndex, length }) => (
         <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
           {new Array(length).fill("").map((_, i) => (
@@ -25,20 +24,20 @@ export function CarouselCustomNavigation() {
       <img
         src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
         alt="image 1"
-        className="object-cover"
+        className="object-contain"
 
   
       />
       <img
         src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
         alt="image 2"
-        className="object-cover"
+        className="object-contain"
 
       />
       <img
         src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
         alt="image 3"
-        className="object-cover"
+        className="object-contain"
 
       />
     </Carousel>
